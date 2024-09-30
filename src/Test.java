f9c161aa269beef46adc552b6c7894df5c3cb230
hulkhongan300 <12calistoafonso@gmail.com>
2024-09-30 10:24:00 +0100

    Add a simple Switch case with a scanner this implomentation should work also this is my first commit in this branch to see if the setting that I intserted worked

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        short z = s.nextShort();
+        System.out.println("Welcome to the [Insert name of the park here]");
+        while (z!=9){
+            switch (z){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
 
+            }
+        }
     }
 }
