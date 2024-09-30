62f527199644faa5b0cbab5829de024ce4d3eaa6
hulkhongan300 <12calistoafonso@gmail.com>
2024-09-30 10:39:54 +0100

    Added a simple Student into the test class This commit is just to see if the merger of all of the branch did work also this class test is going to be change into main since we as a team have not taken a desiscion on how to creat a test suit

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        Student test = new Student("34UI34","Alberto Henrique Cimento",59602);
+
+
         Scanner s = new Scanner(System.in);
+
         short z = s.nextShort();
         System.out.println("Welcome to the [Insert name of the park here]");
         while (z!=9){
