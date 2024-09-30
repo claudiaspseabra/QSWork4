463cd0cacc6853b9d7549126ccbb976c3fb1f4af
Francisco <franciscomarcelo4001@gmail.com>
2024-09-30 10:20:55 +0100

    Student class.

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,42 @@ public class Student {
     private String name;
     private int num;
 
+    public Student(String licensePlate, String name, int num) {
+        this.licensePlate = licensePlate;
+        this.name = name;
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "licensePlate='" + licensePlate + '\'' +
+                ", name='" + name + '\'' +
+                ", num=" + num +
+                '}';
+    }
 }
